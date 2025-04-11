248bc5390ee1a6b7aeeb471f8ab5e4d6f1e07019
Trần Gia Huy <145040043+HUYTG17@users.noreply.github.com>
2025-04-11 21:41:41 +0700

    Create 121.Best_Time_to_Buy_and_Sell_Stock..cpp

---
+++ Leetcode/121.Best_Time_to_Buy_and_Sell_Stock..cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min_buy = prices[0];
+        int max_profit = 0;
+        for(int i = 1; i < prices.size(); ++i)
+        {
+            min_buy = min(min_buy, prices[i]);
+            max_profit = max(max_profit, prices[i] - min_buy);
+        }
+        return max_profit;
+    }
+};
