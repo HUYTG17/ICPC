7cfb625aca76fb6dbd1f031d1691cfd52031c80f
Trần Gia Huy <145040043+HUYTG17@users.noreply.github.com>
2025-05-03 10:52:56 +0700

    Create CD10_BANHOC.cpp

---
+++ Upcoder/CD10_BANHOC.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    int ans = (a + 1) / 2 + (b + 1) / 2 + (c + 1) / 2; // chia từng lớp mới đúng gom lại chia có thể sai nha 
+    cout << ans;
+}
